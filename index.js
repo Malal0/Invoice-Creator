37eea2951e606f5a5b6c402869041d0c3a3afa8e
UncomfortableChai <mj1055119@gmail.com>
2023-02-03 01:27:40 -0800

    added javascript

--- index.js
+++ index.js
@@ -6,6 +6,30 @@
     FUNCTIONS
 /////////////////////*/
 
+function handleClick(e) {
+    if (e.target.parentNode.id === "task-btns-container") {
+        addObject();
+    } else if (e.target.parentNode.parentNode.id === "tasks-container") {
+        removeObject();
+    } else if (e.target.id === "send-invoice-btn") {
+        sendInvoice();
+    }
+}
+
+function addObject() {
+    console.log("object added");
+}
+
+function removeObject() {
+    console.log("object removed");
+}
+
+function sendInvoice() {
+    console.log("Invoice sent");
+}
+
 /*/////////////////////
     EVENT LISTENER
 /////////////////////*/
+
+document.addEventListener("click", handleClick);
